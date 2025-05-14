ac55d67c8444c4f97bc03370a598360d645eac1a
Your Name <you@example.com>
2025-05-14 10:48:47 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -65,7 +65,7 @@ List* get_adj_nodes(Node* n){
       if (pos_vacia_i != -1) break ; 
       
     }
-    
+    if (pos_vacia_i == -1) return NULL ;
     for(int i = 1 ; i <= 9; i++){
       Node * sudo_adj = createNode() ;
       sudo_adj = copy(n) ; 
