be839880acf46d3c6ec26f96a8fe6d0fd040368c
Your Name <you@example.com>
2025-05-14 22:44:46 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -76,11 +76,12 @@ int is_valid(Node* n){
           for (int f = 0; f < 3; f++) {
             for (int c = 0; c < 3; c++) {
                   unsigned int valor = n->sudo[region_fila + f][region_columna + c] ; 
-                  printf("Holi") ;
+                  printf("%d ", valor) ;
                   if (n->sudo[valor] == 0) continue ;
                   contador[valor] += 1;
                   if (contador[valor] == 2) return 0 ;
               }
+            printf("\n") ;
           }
       }
   }
